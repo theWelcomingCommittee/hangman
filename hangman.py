ca76c32a0d05e38200fac549311edd03ae5c6ccf
theWC <77373507+theWelcomingCommittee@users.noreply.github.com>
2022-07-06 23:17:49 -0700

    add colors galore and cleaned up some lines of script

--- hangman.py
+++ hangman.py
@@ -2,6 +2,9 @@ import random
 import os
 import requests
 import itertools
+import colorama
+from colorama import Fore, Style
+colorama.init()
 
 # code begins here
 
@@ -21,7 +24,7 @@ def print_hangman(values):
     print("\t  {} {}      |".format(values[5], values[6]))
     print("\t           |")
     print("    ______________/|\\___")
-    print("  ````'```''`````'``'`''``'``")
+    print(Fore.GREEN + "  ````'```''`````'``'`''``'``" + Style.RESET_ALL)
     print()
 
 
@@ -31,11 +34,15 @@ def print_hangman_win():
     print("\t           |")
     print("\t           |")
     print("\t           |")
-    print("\t   O       |")
-    print("\t  /|\\      |")
-    print("\t   |       |")
-    print("    ______/_\\_____/|\\___")
-    print("  ````'```''`````'``'`''``'``")
+    print(Fore.LIGHTYELLOW_EX + Style.BRIGHT +
+          "\t   O" + Style.RESET_ALL + "       |")
+    print(Fore.LIGHTYELLOW_EX + Style.BRIGHT +
+          "\t  /|\\" + Style.RESET_ALL + "      |")
+    print(Fore.LIGHTYELLOW_EX + Style.BRIGHT +
+          "\t   |" + Style.RESET_ALL + "       |")
+    print("    ______" + Fore.LIGHTYELLOW_EX + Style.BRIGHT + "/" + Style.RESET_ALL +
+          "_" + Fore.LIGHTYELLOW_EX + Style.BRIGHT + "\\" + Style.RESET_ALL + "_____/|\\___")
+    print(Fore.GREEN + "  ````'```''`````'``'`''``'``" + Style.RESET_ALL)
     print()
 
 
@@ -62,22 +69,17 @@ def check_win(values):
 def hangman_game(word):
 
     clear()
-
     # stores letters of word to display
     word_display = []
-
     # store correct letters
     correct_letters = []
-
     # store incorrect letters chosen
     incorrect_letters = []
-
     # number of incorrect guessed chances
     chances = 0
-
     # hangman's body values
-    hangman_values = ['O', '/', '|', '\\', '|', '/', '\\']
-
+    hangman_values = [Fore.LIGHTCYAN_EX + 'O' + Style.RESET_ALL, Fore.LIGHTCYAN_EX + '/' + Style.RESET_ALL, Fore.LIGHTCYAN_EX +
+                      '|' + Style.RESET_ALL, Fore.LIGHTCYAN_EX + '\\' + Style.RESET_ALL, Fore.LIGHTCYAN_EX + '|' + Style.RESET_ALL, Fore.LIGHTCYAN_EX + '/' + Style.RESET_ALL, Fore.LIGHTCYAN_EX + '\\' + Style.RESET_ALL]
     # hangman values before chosen
     show_hangman_values = [' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
@@ -86,14 +88,13 @@ def hangman_game(word):
         if char.isalpha():
             word_display.append('_')
             correct_letters.append(char.upper())
-        # add a if char.isspace()
+        # add a if char.isspace()?
             # word_display.append('  ')
         else:
             word_display.append(char)
 
     # functions for each game
     while True:
-
         # print hangman display
         print_hangman(show_hangman_values)
         print_word(word_display)
@@ -106,19 +107,25 @@ def hangman_game(word):
 
         if len(inp) != 1:
             clear()
-            print('not a choice... try again')
+            #print('not a choice... try again')
+            print(Fore.LIGHTRED_EX +
+                  '  not a choice... try again' + Style.RESET_ALL, end='\r', flush=True)
             continue
 
     # check if alpha (can we combine with above with an OR?)
         if not inp[0].isalpha():
             clear()
-            print('not a choice... try again')
+            #print('not a choice... try again')
+            print(Fore.LIGHTRED_EX +
+                  '  not a choice... try again' + Style.RESET_ALL, end='\r', flush=True)
             continue
 
         # if tried before
         if inp.upper() in incorrect_letters:
             clear()
-            print('you already done picked that! try again!')
+            #print('you already done tried that! try again!')
+            print(Fore.LIGHTRED_EX + '  you already done tried that! try again!' + Style.RESET_ALL,
+                  end='\r', flush=True)
             continue
 
          # incorrect input
@@ -126,15 +133,17 @@ def hangman_game(word):
             incorrect_letters.append(inp.upper())
             # update hangman
             show_hangman_values[chances] = hangman_values[chances]
-            chances = chances + 1
+            chances += 1
 
             # check if lost
             if chances == len(hangman_values):
                 print()
                 clear()
                 print_hangman(hangman_values)
-                print("   YOU GOT HUNG!!! GAME OVER!")
-                print("the word that your dead ass got wrong was:", word.upper())
+                print(Fore.RED + Style.BRIGHT +
+                      "   YOU GOT HUNG!!! GAME OVER!" + Style.RESET_ALL)
+                print("the word that your dead ass got wrong was:",
+                      Fore.LIGHTCYAN_EX + word.upper() + Style.RESET_ALL)
                 break
 
         # correct input
@@ -147,8 +156,10 @@ def hangman_game(word):
             if check_win(word_display):
                 clear()
                 print_hangman_win()
-                print("   CONGRATULATIONS!!! YOU'RE NOT DEAD YET!")
-                print("       the word was:", word.upper())
+                print(
+                    Fore.GREEN + "   CONGRATULATIONS!!! YOU'RE NOT DEAD YET!" + Style.RESET_ALL)
+                print("       the word was:", Fore.LIGHTYELLOW_EX +
+                      word.upper() + Style.RESET_ALL)
                 break
         clear()
 
@@ -156,6 +167,7 @@ def hangman_game(word):
 if __name__ == "__main__":
     clear()
 
+    '''possibly future links to csv or text files'''
     # categories
     topics = {1: "animals", 2: "outerspace", 3: "sports",
               4: "any word from these topics", 5: "literally any english word"}
@@ -170,38 +182,34 @@ if __name__ == "__main__":
         # game menu
         print()
         print("-----------------------------")
-        print("\tGAME MENU")
+        print("    HANGMAN GAME MENU")
         print("-----------------------------")
         print()
         print("choose a topic:")
         for key in topics:
             print("PRESS ", key, " to select", topics[key])
-        print("PRESS ",  len(topics)+1, " to quit")
+        print("PRESS  6  to quit")
         print()
 
         ran = ()
 
-        # choosing topics
+        # choosing topic
         try:
             choice = int(input("Enter your choice = "))
 
-        # checking the input
-        except ValueError:
+        # check input
+        except (ValueError or choice < 0 or choice > 6):
             clear()
             print("not a choice... Try Again")
             continue
 
-        if choice > len(topics)+1:
-            clear()
-            print("not even close to one of the choices... Try again")
-            continue
-
-        elif choice < 4:
-            chosenTopic = topics[choice]
-            ran = random.choice(dataset[chosenTopic])
+        # if choice < 1 or choice > 6:
+            # clear()
+            #print("not even close to one of the choices... Try again")
+            # continue
 
         # making list of wild card list of all words from topics
-        elif choice == 4:
+        if choice == 4:
             res_list = []
             for key, value in (itertools.chain.from_iterable([itertools.product((k, ), v) for k, v in dataset.items()])):
                 res_list.append(value)
@@ -214,11 +222,16 @@ if __name__ == "__main__":
             ran = res.strip('["]')
 
         # exit choice
-        elif choice == len(topics)+1:
+        elif choice == 6:
             print()
             print("---------------------")
             print(" Thanks for playin'!")
             break
+
+        # pick of the topics
+        else:
+            chosenTopic = topics[choice]
+            ran = random.choice(dataset[chosenTopic])
 
         # run game function
         hangman_game(ran)
